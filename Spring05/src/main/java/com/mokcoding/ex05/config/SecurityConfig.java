915f8fa21b49f8a996badab9e44c83a85758bea7
jhmoc <jhmoc@DESKTOP-J5VP8LR>
2024-03-30 20:37:53 +0900

    merge

--- Spring05/src/main/java/com/mokcoding/ex05/config/SecurityConfig.java
+++ Spring05/src/main/java/com/mokcoding/ex05/config/SecurityConfig.java
@@ -11,15 +11,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	// HttpSecurity 객체를 통해 HTTP 보안을 구성
+ 
+	// HttpSecurity 객체를 통해 HTTP 보안 기능을 구성
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		
-		// HTTP 보안 구성
 		httpSecurity
-		.authorizeRequests() // 요청에 대한 권한 부여
-		// /example/main에  대한 모든 사용자 접근을 허용
+		.authorizeRequests()
+		// /example/main에 대한 모든 사용자 접근을 허용
 		.antMatchers("/example/main").permitAll() 
 		// /example/admin에 대한 ADMIN 사용자 접근을 허용
 		.antMatchers("/example/admin").access("hasRole('ROLE_ADMIN')")  
@@ -35,13 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		httpSecurity.formLogin(); // 기본 formLogin 기능 사용
 	}
 
-	// AuthenticationManagerBuilder 객체를 통해 인증을 구성
+	// AuthenticationManagerBuilder 객체를 통해 인증 기능을 구성
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication() // 애플리케이션 메모리에 사용자 정보 저장
-		// user : member1, password : 1234, role : MEMBER
-		.withUser("member1").password("{noop}1234").roles("MEMBER") 
-		// user : admin1, password : 1234, role : ADMIN
+//		 user : member1, password : 1234, role : MEMBER
+		.withUser("member1").password("{noop}1234").roles("MEMBER")
+//		 user : admin1, password : 1234, role : ADMIN
 		.and().withUser("admin1").password("{noop}1234").roles("ADMIN");
 		
 		// noop : 암호 인코딩을 설정하지 않음을 의미
