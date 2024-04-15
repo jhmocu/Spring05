62bd60c58dec916c759644242ca4161961fcd0ad
jhmocu <jhmocu@gmail.com>
2024-04-15 18:57:26 +0900

    add comment

--- Spring05/src/main/java/com/mokcoding/ex05/config/SecurityConfig.java
+++ Spring05/src/main/java/com/mokcoding/ex05/config/SecurityConfig.java
@@ -17,40 +17,38 @@ import com.mokcoding.ex05.service.CustomUserDetailsService;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	// 비밀번호 암호화를 위한 BCryptPasswordEncoder를 빈으로 생성
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
- 
-    
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	// HttpSecurity 객체를 통해 HTTP 보안 기능을 구성
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		
-		httpSecurity
-		.authorizeRequests()
-		// "/example/main" URL에 대한 모든 사용자 접근을 허용
-		.antMatchers("/example/main").permitAll() 
-		// "/example/admin" URL에 대한 ADMIN 사용자 접근을 허용
-		.antMatchers("/example/admin").access("hasRole('ROLE_ADMIN')")  
-		// "/example/admin" URL에 대한 MEMBER 사용자 접근을 허용
-		.antMatchers("/example/member").access("hasRole('ROLE_MEMBER')");
-		
+
+		httpSecurity.authorizeRequests()
+				// "/example/main" URL에 대한 모든 사용자 접근을 허용
+				.antMatchers("/example/main").permitAll()
+				// "/example/admin" URL에 대한 ADMIN 사용자 접근을 허용
+				.antMatchers("/example/admin").access("hasRole('ROLE_ADMIN')")
+				// "/example/admin" URL에 대한 MEMBER 사용자 접근을 허용
+				.antMatchers("/example/member").access("hasRole('ROLE_MEMBER')");
+
 		// antMatchers(pattern) : 특정 url 패턴에 맞는 경로 매핑
 		// permitAll() : 모든 사용자 접근
 		// access() : 특정 권한을 가진 사용자 접근
 		// hasRole('ROLE_XXX') : XXX 등급으로 권한 설정
-	
+
 		// 접근 제한 경로 설정
 		httpSecurity.exceptionHandling().accessDeniedPage("/access/accessDenied");
-		
+
 		httpSecurity.formLogin().loginPage("/access/login"); // 커스텀 로그인 url 설정
-				
+
 		httpSecurity.logout().logoutUrl("/access/logout") // logout url 설정
-		.invalidateHttpSession(true); // 세션 무효화 설정
-		
+				.invalidateHttpSession(true); // 세션 무효화 설정
+
 		// header 정보에 xssProtection 기능 설정
 		httpSecurity.headers().xssProtection().block(true);
 		httpSecurity.headers().contentSecurityPolicy("script-src 'self'");
@@ -59,13 +57,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// AuthenticationManagerBuilder 객체를 통해 인증 기능을 구성
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService());
+		auth.userDetailsService(userDetailsService()); // CustomUserDetailsService 빈 적용
 	}
-	
-    // 사용자 정의 로그인 클래스인 CustomUserDetailsService를 빈으로 생성
-    @Bean
-    public UserDetailsService userDetailsService() {
-    	return new CustomUserDetailsService();
-    }
+
+	// 사용자 정의 로그인 클래스인 CustomUserDetailsService를 빈으로 생성
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomUserDetailsService();
+	}
 
 }
